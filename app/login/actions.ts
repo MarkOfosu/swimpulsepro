4b9d2d58639b50ba6b6efe52a57375543d683fb9
MarkOfosu <124046783+MarkOfosu@users.noreply.github.com>
2024-08-13 06:33:15 -0700

    wired up supabase sign up and login flow

--- app/login/actions.ts
+++ app/login/actions.ts
@@ -21,26 +21,10 @@ export async function login(formData: FormData) {
     redirect('/error')
   }
 
+
+
   revalidatePath('/', 'layout')
-  redirect('/')
+  //for now redirect to coach dashboard
+  redirect('/user/coach/dashboard')
+ 
 }
-
-// export async function signup(formData: FormData) {
-//   const supabase = createClient()
-
-//   // type-casting here for convenience
-//   // in practice, you should validate your inputs
-//   const data = {
-//     email: formData.get('email') as string,
-//     password: formData.get('password') as string,
-//   }
-
-//   const { error } = await supabase.auth.signUp(data)
-
-//   if (error) {
-//     redirect('/error')
-//   }
-
-//   revalidatePath('/', 'layout')
-//   redirect('/')
-// }
