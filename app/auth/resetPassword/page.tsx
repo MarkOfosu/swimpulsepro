56c93f786f8dbd45e434a2c03d969071326be3ec
MarkOfosu <124046783+MarkOfosu@users.noreply.github.com>
2024-11-13 01:10:25 -0800

    added susspense to reset password because of of useSearchParams

--- app/auth/resetPassword/page.tsx
+++ app/auth/resetPassword/page.tsx
@@ -1,16 +1,18 @@
+import { Suspense } from 'react';
 import WelcomeNavbar from '@app/welcome/WelcomeNavbar';
 import ResetPassword from './ResetPassword';
 import Footer from '@components/elements/Footer';
 
 const ResetPasswordPage = () => {
-    return (
-        <>
-            <WelcomeNavbar />
-                <ResetPassword />
-            <Footer />
-        </>
-    );
-    };
+  return (
+    <>
+      <WelcomeNavbar />
+      <Suspense fallback={<div>Loading...</div>}>
+        <ResetPassword />
+      </Suspense>
+      <Footer />
+    </>
+  );
+};
 
 export default ResetPasswordPage;
-
