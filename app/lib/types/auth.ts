0e2c2e8457cf48341d6de57d57bc684c75c02ecb
MarkOfosu <124046783+MarkOfosu@users.noreply.github.com>
2024-11-27 01:27:01 -0800

    installed zustand for state management

--- app/lib/types/auth.ts
+++ app/lib/types/auth.ts
@@ -5,3 +5,28 @@ export interface AuthError extends Error {
   status: number;
   code: string;
 }
+
+export interface BaseUserData {
+    id: string;
+    first_name: string;
+    last_name: string;
+    email: string;
+    role: 'swimmer' | 'coach';
+    date_of_birth: Date | null;
+    gender?: string;
+    isVerified?: boolean;
+  }
+  
+  export interface SwimmerData extends BaseUserData {
+    role: 'swimmer';
+    group_id?: string;
+    group_name?: string;
+    coach_first_name?: string;
+    coach_last_name?: string;
+  }
+  
+  export interface CoachData extends BaseUserData {
+    role: 'coach';
+    team_name?: string;
+    team_location?: string;
+  }
